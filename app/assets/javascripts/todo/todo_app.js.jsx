21fb9b48fee8229b4a6885c7e487e12c1fa983c2
Andrey Savelyev <savelyev.andrey@gmail.com>
2014-01-11 15:57:45 +0400

    editing

--- app/assets/javascripts/todo/todo_app.js.jsx
+++ app/assets/javascripts/todo/todo_app.js.jsx
@@ -3,8 +3,8 @@ var TodoApp = React.createClass({
   getInitialState: function() {
     return {
       items: [
-        { text: "First item", checked: false, id: 1 },
-        { text: "Second item", checked: true, id: 2 }
+        { text: "First item", checked: false, id: 1, edit_text: null },
+        { text: "Second item", checked: true, id: 2, edit_text: null }
       ],
       text: ""
     };
@@ -16,7 +16,6 @@ var TodoApp = React.createClass({
     var nextItems = this.state.items.concat([{ text: this.state.text, checked: false, id: this.state.items.length, edit: false }]);
 
     this.setState({ items: nextItems, text: ''});
-    console.debug(this.state.items);
   },
 
   onChange: function(e) {
@@ -38,7 +37,20 @@ var TodoApp = React.createClass({
   handleEdit: function(item) {
     // TODO: Сделать нормально
     item.edit = !item.edit;
+    item.edit_text = item.text
     this.setState({});
+  },
+
+  handleChangeItem: function(item, newName) {
+    item.edit_text = newName;
+    this.setState({items: this.state.items});
+  },
+
+  updateSubmitHandle: function(item) {
+    item.text = item.edit_text;
+    item.edit = !item.edit;
+    this.setState({});
+    return false;
   },
 
   render: function() {
@@ -48,7 +60,9 @@ var TodoApp = React.createClass({
           item={item}
           handleCheck={this.handleCheck.bind(this, item)}
           handleEdit={this.handleEdit.bind(this, item)}
-          handleDelete={this.handleDelete.bind(this, item)} />
+          handleDelete={this.handleDelete.bind(this, item)}
+          handleChangeItem={this.handleChangeItem.bind(this, item)}
+          updateSubmitHandle={this.updateSubmitHandle.bind(this, item)} />
       );
     };
 
@@ -71,7 +85,11 @@ var TodoApp = React.createClass({
 });
 
 var TodoItem = React.createClass({
+  handleChange:  function(e) {
+    this.props.handleChangeItem(e.target.value)
+  },
   render: function() {
+    var tmpName = null;
     var cx = React.addons.classSet;
     var classes = cx({
       'completed': this.props.item.checked
@@ -79,9 +97,8 @@ var TodoItem = React.createClass({
     if (this.props.item.edit) {
       return (
         <li>
-          <input type="checkbox" onChange={this.props.handleCheck} checked={this.props.item.checked} />
-          <form>
-            <input value={this.props.item.text} />
+          <form onSubmit={this.props.updateSubmitHandle}>
+            <input onChange={this.handleChange} value={this.props.item.edit_text} />
             <button>Save</button>
           </form>
         </li>
@@ -99,7 +116,6 @@ var TodoItem = React.createClass({
     }
   }
 });
-
 
 $(function() {
   React.renderComponent(<TodoApp />, document.getElementById('todo_app'));
